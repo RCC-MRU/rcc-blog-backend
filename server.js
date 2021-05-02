7481f4ed48d8787fcf27e50e3f4884885b24ac3d
Sanchit <sanchit02@outlook.com>
2021-05-02 17:14:13 +0530

    added axios

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require('cors');
 const port = process.env.PORT || 3001;
 
 // middlewares
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 // Calling database and authenticate
 const db = require("./database/db");
