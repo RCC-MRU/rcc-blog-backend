f79aa0c57506e6a28884a95f26da9b119c8b1311
Sanchit <sanchit02@outlook.com>
2022-03-14 09:20:59 +0530

    updated db

--- database/db.js
+++ database/db.js
@@ -3,10 +3,14 @@ dotenv.config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.HOST || "localhost",
-  user: process.env.DB_USERNAME || "root",
-  password: process.env.PASSWORD || "",
-  database: process.env.DATABASE || "rccblog",
+  // host: process.env.HOST || "localhost",
+  // user: process.env.DB_USERNAME || "root",
+  // password: process.env.PASSWORD || "",
+  // database: process.env.DATABASE || "rccblog",
+  host: "rccblog.mysql.database.azure.com",
+  user: "rcc",
+  password: "Lucky1033#$%",
+  database: "rccblog",
 });
 
 module.exports = db;
